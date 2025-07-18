07c39f216e487866be51f86b16df0976085841ac
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2025-07-18 21:17:46 +0200

    init

---
+++ src/main/java/pl/vvhoffmann/lotteryapp/LotteryAppApplication.java
@@ -0,0 +1,13 @@
+package pl.vvhoffmann.lotteryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/vvhoffmann/lotteryapp/LotteryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.vvhoffmann.lotteryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
