5d36baf9a09dd1864b2fbe7ff8922171f1a7ed83
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2025-07-18 23:11:24 +0200

    prepared Number Receiver Facade Test

--- src/main/java/pl/vvhoffmann/lotteryapp/domain/numberreceiver/NumberReceiverFacade.java
+++ src/main/java/pl/vvhoffmann/lotteryapp/domain/numberreceiver/NumberReceiverFacade.java
@@ -1,5 +1,17 @@
 package pl.vvhoffmann.lotteryapp.domain.numberreceiver;
 
 
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class NumberReceiverFacade {
+
+    public String inputNumbers(Set<Integer> numbers)
+    {
+        final Set<Integer> verifiedNumbers = numbers.stream()
+                .filter(number -> number >= 1)
+                .filter(number -> number <= 99)
+                .collect(Collectors.toSet());
+        return verifiedNumbers.size() == 6 ? "success" : "failed";
+    }
 }
--- src/test/java/pl/vvhoffmann/lotteryapp/domain/numberreceiver/NumberReceiverFacadeTest.java
+++ src/test/java/pl/vvhoffmann/lotteryapp/domain/numberreceiver/NumberReceiverFacadeTest.java
@@ -1,7 +1,55 @@
 package  pl.vvhoffmann.lotteryapp.domain.numberreceiver;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class NumberReceiverFacadeTest {
+    @Test
+    @DisplayName("Should return success when user gave 6 numbers")
+    public void should_return_success_when_user_gave_six_numbers() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        final String result = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5, 6));
+        //then
+        assertThat(result).isEqualTo("success");
+    }
+
+    @Test
+    @DisplayName("Should return failed when user gave less than 6 numbers")
+    public void should_return_failed_when_user_gave_less_than_six_numbers() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        final String result = numberReceiverFacade.inputNumbers(Set.of(2, 3, 4, 5, 6));
+        //then
+        assertThat(result).isEqualTo("failed");
+    }
+
+    @Test
+    @DisplayName("Should return failed when user gave more than 6 numbers")
+    public void should_return_failed_when_user_gave_more_than_six_numbers() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        final String result = numberReceiverFacade.inputNumbers(Set.of(1, 2, 3, 4, 5, 6, 7));
+        //then
+        assertThat(result).isEqualTo("failed");
+    }
+
+    @Test
+    @DisplayName("Should return failed when user gave at least one number out of range 1-99")
+    public void should_return_failed_when_user_gave_at_least_one_number_out_of_range_1_to_99() {
+        //given
+        NumberReceiverFacade numberReceiverFacade = new NumberReceiverFacade();
+        //when
+        final String result = numberReceiverFacade.inputNumbers(Set.of(1, 2, 30, 4, 500, 6));
+        //
+        assertThat(result).isEqualTo("failed");
+    }
 
 }
