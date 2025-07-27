428ef4e77ec2bb62e230f9292ecc0920894bd22a
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2025-07-27 13:37:03 +0200

    added error validation integration test

---
+++ integration/java/pl/vvhoffmann/lotteryapp/apivalidationerror/ApiValidationFailedIntegrationTest.java
@@ -0,0 +1,58 @@
+package pl.vvhoffmann.lotteryapp.apivalidationerror;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import pl.vvhoffmann.lotteryapp.BaseIntegrationTest;
+import pl.vvhoffmann.lotteryapp.infrastructure.apivalidation.dto.ApiValidationErrorDto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class ApiValidationFailedIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void should_return_400_bad_request_and_validation_message_when_request_doesnt_have_input_numbers() throws Exception {
+        //given
+
+        //when
+        final ResultActions perform = mockMvc.perform(post("/inputNumbers")
+                .content("""
+                        {
+                        "inputNumbers": []
+                        }
+                        """.trim()
+                ).contentType(MediaType.APPLICATION_JSON)
+        );
+        //then
+        MvcResult mvcResult = perform.andExpect(status().isBadRequest()).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        final ApiValidationErrorDto result = objectMapper.readValue(json, ApiValidationErrorDto.class);
+        assertThat(result.messages()).containsExactlyInAnyOrder(
+                "Input numbers must not be empty");
+
+    }
+
+    @Test
+    public void should_return_400_bad_request_and_validation_message_when_request_is_empty() throws Exception {
+        //given
+
+        //when
+        final ResultActions perform = mockMvc.perform(post("/inputNumbers")
+                .content("""
+                        {}
+                        """.trim()
+                ).contentType(MediaType.APPLICATION_JSON)
+        );
+        //then
+        MvcResult mvcResult = perform.andExpect(status().isBadRequest()).andReturn();
+        String json = mvcResult.getResponse().getContentAsString();
+        final ApiValidationErrorDto result = objectMapper.readValue(json, ApiValidationErrorDto.class);
+        assertThat(result.messages()).containsExactlyInAnyOrder(
+                "Input numbers must not be null",
+                "Input numbers must not be empty");
+
+    }
+}
