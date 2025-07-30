43501de16260c57b7b25315817ab4efb6b082c47
Wiktoria Hoffmann <whoffmann2000@wp.pl>
2025-07-30 09:21:31 +0200

    added next errors integration tests

--- integration/java/http/numbergenerator/RandomNumbersGeneratorRestTemplateErrorsIntegrationTest.java
+++ integration/java/http/numbergenerator/RandomNumbersGeneratorRestTemplateErrorsIntegrationTest.java
@@ -5,6 +5,7 @@ import com.github.tomakehurst.wiremock.http.Fault;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 import pl.vvhoffmann.lotteryapp.domain.numbersgenerator.RandomNumbersGenerable;
 
@@ -95,7 +96,90 @@ public class RandomNumbersGeneratorRestTemplateErrorsIntegrationTest {
          assertThat(throwable.getMessage()).isEqualTo("500 INTERNAL_SERVER_ERROR");
      }
 
+    @Test
+    public void should_return_zero_job_offers__when_status_is_204_no_content()
+    {
+        //given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                    .withStatus(HttpStatus.NO_CONTENT.value())
+                    .withBody("""
+                            [1, 2, 3, 4, 5, 6, 82, 82, 83, 83, 86, 57, 10, 81, 53, 93, 50, 54, 31, 88, 15, 43, 79, 32, 43]
+                            """.trim()
+                    )
+                    .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                ));
 
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("204 NO_CONTENT");
+    }
+
+    @Test
+    void should_return_zero_job_offers_when_response_delay_is_5000_ms_and_client_has_1000ms_read_timeout() {
+        //given
+        int readTimeout = 1000;
+        int connectTimeout = 2000;
+        RandomNumbersGenerable randomNumbersGenerable = new RandomNumbersGeneratorRestTemplateIntegrationTestConfig().remoteNumberGeneratorClient(
+                wireMockServer.getPort(),
+                connectTimeout,
+                readTimeout
+        );
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withBody("""
+                            [1, 2, 3, 4, 5, 6, 82, 82, 83, 83, 86, 57, 10, 81, 53, 93, 50, 54, 31, 88, 15, 43, 79, 32, 43]
+                            """.trim()
+                        )
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withFixedDelay(5000)
+                ));
+
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("500 INTERNAL_SERVER_ERROR");
+    }
+
+    @Test
+    void should_return_response_not_found_status_exception_when_http_service_returning_not_found_status() {
+        // given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withStatus(HttpStatus.NOT_FOUND.value()))
+        );
+
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("404 NOT_FOUND");
+    }
+
+    @Test
+    void should_return_response_unauthorized_status_exception_when_http_service_returning_unauthorized_status() {
+        // given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withStatus(HttpStatus.UNAUTHORIZED.value()))
+        );
+
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("401 UNAUTHORIZED");
+    }
 
 
 
